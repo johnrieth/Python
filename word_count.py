1ad5afd3a5ed55df8b00b819d8511cfbfb473b0c
0x15 <john@0x15.page>
2024-03-26 22:22:54 -0700

    adding word count program

---
+++ word_count.py
@@ -0,0 +1,14 @@
+# a word-count program
+
+
+# input is a line of text
+line = input()
+
+# remove leading and trailing spaces
+line = line.strip()
+
+# count number of words
+total_words = line.count(' ') + 1
+
+# output is the number of words in the line of text
+print(total_words)
